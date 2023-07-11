62d1c3e4a3b7ccc20ec9d1eab130bfc2638c2db0
mqthang_18sh <maiquocthangt@gmail.com>
2023-07-11 19:49:11 +0700

    Update

--- data/content/HQTCSDL/HQTCSDL4.js
+++ data/content/HQTCSDL/HQTCSDL4.js
@@ -1,14 +1,319 @@
 const HQTCSDL4 = {
     HQTCSDL01: {
-        title: '',
+        title: 'An toàn dữ liệu',
         index: [
+            ["Phân loại sự cố", "HQTCSDL011", `
+            <center>
+              <video class="col-12 col-sm-8" controls>
+                <source
+                  src="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P1.mp4#t=0,677"
+                  type="video/mp4" />
+              </video>
+            </center>
+            <div>
+              <p>
+                <a class="btn btn-primary mt-2 mb-2" data-bs-toggle="collapse" href="#collapse111" role="button"
+                  aria-expanded="false" aria-controls="collapse111">
+                  Giới thiệu
+                </a>
+                <a class="btn btn-primary mt-2 mb-2" data-bs-toggle="collapse" href="#collapse112" role="button"
+                  aria-expanded="false" aria-controls="collapse112">
+                  Sự cố do nhập liệu sai
+                </a>
+                <a class="btn btn-primary mt-2 mb-2" data-bs-toggle="collapse" href="#collapse113" role="button"
+                  aria-expanded="false" aria-controls="collapse113">
+                  Sự cố trên thiết bị lưu trữ
+                </a>
+                <a class="btn btn-primary mt-2 mb-2" data-bs-toggle="collapse" href="#collapse114" role="button"
+                  aria-expanded="false" aria-controls="collapse114">
+                  Sự cố của giao tác
+                </a>
+                <a class="btn btn-primary mt-2 mb-2" data-bs-toggle="collapse" href="#collapse115" role="button"
+                  aria-expanded="false" aria-controls="collapse115">
+                  Sự cố hệ thống
+                </a>
+              </p>
+              <div>
+                <div class="collapse" id="collapse111">
+                  <div class="card card-body">
+                    <p>CSDL luôn cần phải đảm bảo tất cả các ràng buộc toàn vẹn <b>(RBTV)</b></p>
+                    <p>Nguyên nhân dẫn đến RBTV bị vi phạm:</p>
+                    <ul>
+                      <li>Do <b style="color: red;">chia sẻ dữ liệu</b></li>
+                      <li>Do <b style="color: red;">sự cố</b>
+                        <ul>
+                          <li>Nhập dữ liệu sai</li>
+                          <li>Hư hỏng phần cứng</li>
+                          <li>Lỗi hệ thống</li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="collapse" id="collapse112">
+                  <div class="card card-body">
+                    <ul>
+                      <li><b style="color: red;">Dữ liệu sai hiển nhiên</b>: Là sự nhập sai dữ liệu mà máy tính có thể
+                        phát hiện
+                        <br>
+                        - Vd: Nhập thiếu 1 số trong dãy số điện thoại...
+                      </li>
+                      <li><b style="color: red;">Dữ liệu sai không hiển nhiên</b>: Là sự nhập sai dữ liệu liên quan đến
+                        ngữ nghĩa mà máy tính khó có thể tự nó phát hiện được
+                        <br>
+                        - Vd: Nhập sai 1 số trong dãy số điện thoại
+                      </li>
+                    </ul>
+                    <center>
+                      <img src="./assets/img/HQTCSDL/chap4/img1.jpeg" alt=""
+                        class="col-12 col-sm-8">
+                    </center>
+                  </div>
+                </div>
+                <div class="collapse" id="collapse113">
+                  <div class="card card-body">
+                    <ul>
+                      <li>Mất dữ liệu trên thiết bị lưu trữ</li>
+                      <li>Không thể truy xuất dữ liệu từ thiết bị lưu trữ
+                        <ul>
+                          <li>Vd. Cháy nổ gây phá hủy thiết bị lưu trữ...</li>
+                          <li>Vd. Đầu đọc của đĩa cứng hư</li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <center>
+                      <img src="./assets/img/HQTCSDL/chap4/img2.jpeg" alt="" class="col-12 col-sm-8">
+                    </center>
+                  </div>
+                </div>
+                <div class="collapse" id="collapse114">
+                  <div class="card card-body">
+                    <ul>
+                      <li>Sự cố làm cho 1 giao tác kết thúc không bình thường</li>
+                      <li>Ví dụ
+                        <ul style="list-style-type: '-';">
+                          <li>Chia cho 0</li>
+                          <li>Dữ liệu nhập sai</li>
+                          <li>Tràn số</li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <center>
+                      <img src="./assets/img/HQTCSDL/chap4/img3.jpeg" alt="" class="col-12 col-sm-8">
+                    </center>
+                  </div>
+                </div>
+                <div class="collapse" id="collapse115">
+                  <div class="card card-body">
+                    <ul>
+                      <li>Lỗi phần cứng
+                        <ul>
+                          <li>Cúp điện</li>
+                          <li>Hư bộ nhớ trong</li>
+                          <li>Hư CPU</li>
+                        </ul>
+                      </li>
+                      <li>Lỗi phần mềm
+                        <ul>
+                          <li>Lỗi hệ điều hành</li>
+                          <li>Lỗi HQTCSDL</li>
+                        </ul>
+                      </li>
+                    </ul>
+                    <center>
+                      <img src="./assets/img/HQTCSDL/chap4/img4.jpeg" alt="" class="col-12 col-sm-8">
+                    </center>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `],
+            ["Nhật ký giao tác", "HQTCSDL012", `
+            <center>
+              <video class="col-12 col-sm-8" controls>
+                <source
+                  src="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P1.mp4#t=677,1131"
+                  type="video/mp4" />
+              </video>
+            </center>
+            <ul>
+              <li>Nhật ký giao tác là một chuỗi các mẫu tin (<b style="color: red;">log record</b>) ghi lại các hành
+                động của HQT CSDL</li>
+              <li>Nhật ký là một tập tin tuần tự được lưu trữ trên bộ nhớ chính và được ghi xuống đĩa ngay khi có thể
+                <center>
+                  <img src="./assets/img/HQTCSDL/chap4/img5.jpeg" alt="" class="col-12 col-sm-8">
+                </center>
+              </li>
+              <li><b>Flush-log</b>: là hành động chép những block mẫu tin nhất ký mới chưa được chép từ bộ nhớ vào ổ đĩa
+              </li>
+              <li>Nhật ký giao tác được sử dụng trong các phương pháp khôi phục sự cố</li>
+              <li>Mỗi kỹ thuật khôi phục sự cố khác nhau sẽ có một cách ghi nhật ký khác nhau và các cú pháp mẫu tin
+                khác nhau.</li>
+              <li>Một mẫu tin nhật ký có thể là:
+                <center>
+                  <img src="./assets/img/HQTCSDL/chap4/img6.jpeg" alt=""
+                    class="col-12 col-sm-10">
+                </center>
+              </li>
+              <li>Khi sự cố hệ thống xảy ra:
+                <ul>
+                  <li>HQT CSDL sẽ tra cứu nhật ký giao tác để khôi phục lại
+                    trạng thái nhất quán của dữ liệu.</li>
+                  <li>Khi đó:
+                    <ul>
+                      <li>Một vài giao tác sẽ phải thực hiện lại (redo)</li>
+                      <li>Một vài giao tác sẽ phải hủy bỏ (undo)</li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+        
+            `],
+            ["Điểm kiểm tra", "HQTCSDL013", `
             
+            <center>
+              <video class="col-12 col-sm-8" controls>
+                <source
+                  src="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P1.mp4#t=1131"
+                  type="video/mp4" />
+              </video>
+            </center>
+            <div>
+              <ul>
+                <li>HQT CSDL không thể tra cứu toàn bộ nhật ký -> mất nhiều thời gian</li>
+                <li>Giải pháp: Dùng điểm kiểm tra (check point)
+                  <br>
+                  Nhật ký giao tác có thêm mẫu tin &lt;checkpoint&gt; hay &lt;ckpt&gt;
+                </li>
+              </ul>
+              <center>
+                <img src="./assets/img/HQTCSDL/chap4/img7.jpeg" alt="" class="col-12 col-sm-2">
+              </center>
+            </div>
+            <div>
+              <p>
+                <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse131" role="button"
+                  aria-expanded="false" aria-controls="collapse131">
+                  Điểm kiểm tra đơn giản
+                </a>
+                <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse132" role="button"
+                  aria-expanded="false" aria-controls="collapse131">
+                  Điểm kiểm tra linh động
+                </a>
+              </p>
+              <div>
+                <div class="collapse" id="collapse131">
+                  <div class="card card-body">
+                    <div class="container-fluid">
+                      <div class="row">
+                        <div class="col-12 col-sm-6">
+                          <p><b>Khi đến điểm kiểm tra, HQT CSDL sẽ</b></p>
+                          <ol>
+                            <li>Tạm <b style="color: red;">dừng tiếp nhận các giao tác mới</b></li>
+                            <li>Đợi các giao tác đang thực hiện -&gt;ghi mẫu tin &lt;commit&gt; hay &lt;abort T&gt; vào nhật ký</li>
+                            <li>Tiến hành ghi dữ liệu và nhất ký từ vùng đệm xuống đĩa</li>
+                            <li>Tạo 1 mẫu tin &lt;checkpoint&gt; và ghi xuống đĩa</li>
+                            <li>Tiếp tục nhận các giao tác mới</li>
+                          </ol>
+                          <p><b>Khi có sự cố</b></p>
+                          <ul>
+                            <li>HQT CSDL <b style="color: red;">dùng nhật ký giao tác để khôi phục</b></li>
+                            <li>Các giao tác ở phía trước điểm kiểm tra mới nhất là những giao tác đã kết thúc -> không cần làm lại</li>
+                            <li>Các giao tác ở phía sau điểm kiểm tra là những giao tác chứa thực hiện xong -> cần khôi phục</li>
+                          </ul>
+                        </div>
+                        <div class="col-12 col-sm-6">
+                          <center>
+                            <img src="./assets/img/HQTCSDL/chap4/img8.jpeg" alt="" class="col-12 col-sm-8">
+                            <img src="./assets/img/HQTCSDL/chap4/img9.jpeg" alt="" class="col-12 col-sm-8">
+                          </center>
+                        </div>
+                      </div>
+                    </div>
+                    <p style="border: 1px solid; color: blue; font-weight: 600;">
+                      => HQT CSDL sẽ
+                      <br>
+                      Không phải duyệt hết nhật ký
+                      <br>
+                      Chỉ duyệt ngược từ cuối nhất ký đến điểm tra
+                    </p>
+                  </div>
+                </div>
+                <div class="collapse" id="collapse132">
+                  <div class="card card-body">
+                    <ul>
+                      <li>Mẫu tin &lt;checkpoint&gt; trong <i>Điểm kiểm tra đơn giản</i> nay được chia thành 2 mẫu tin
+                        <ul>
+                          <li>Mẫu tin &lt;start ckpt(T<sub>1</sub>, T<sub>2</sub>, T<sub>3</sub>, T<sub>4</sub>)&gt;: Bắt đầu checkpoint T<sub>1</sub>, T<sub>2</sub>,...,T<sub>k</sub> là các giao tác đang dở dang khi bắt đầu checkpoint</li>
+                          <li>Mẫu tin &lt;end ckpt&gt;: Kết thúc checkpoint</li>
+                        </ul>
+                      </li>
+                      <li>Cho phép <b style="color: red;">tiếp nhận các giao tác mới trong quá trình checkpoint</b></li>
+                    </ul>
+                    <p><b>Khi đến điểm kiểm tra, HQT CSDL sẽ</b></p>
+                    <ol>
+                        <li>Tạm ngưng mọi hoạt động</li>
+                        <li>Tạo và ghi mẫu tin &lt;start ckpt (T1, T2,..., Tk)&gt;
+                            <br>
+                            T1 , T2, ...,Tk là những giao tác đang thực thi
+                        </li>
+                        <li>Chờ cho đến khi T1, T2, ...,Tk hoàn tất hay hủy bỏ <br>
+                        nhưng khi ấy không ngăn các giao tác mới bắt đầu</li>
+                        <li>Khi T1, T2, ...,Tk kết thúc, tạo mẫu tin &lt;end ckpt&gt; và ghi xuống đĩa</li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `]
+        ]
+    },
+    HQTCSDL02: {
+        title: 'Khôi phục dữ liệu',
+        index: [
+            ["Undo loging", "HQTCSDL021", `
+                <center>
+                    <video class="col-12 col-sm-10" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P2.mp4#t=0,1740" type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Redo loging", "HQTCSDL022", `
+                <center>
+                    <video class="col-12 col-sm-10" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P2.mp4#t=1740" type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Undo/Redo loging", "HQTCSDL023", `
+            <center>
+            <video class="col-12 col-sm-10" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P3.mp4#t=0,1245" type="video/mp4" />
+            </video>
+        </center>
+            `],
+
+        ]
+    },
+    HQTCSDL03: {
+        title: 'An ninh dữ liệu',
+        index: [
+
         ]
     },
     HQTCSDL04: {
         title: 'Tổng kết',
         index: [
             ["Video bài giảng", "HQTCSDL041", `
+            <script>
+                let vid = document.getElementsByTagName("video");
+                console.log(vid)
+
+                Object.keys(vid).forEach(element => {
+                    vid[element].playbackRate = 1.75
+                });
+            </script>
                 <div>
                     <p>Lý thuyết 1: <a target="__blank" href="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P1.mp4">Click here</a></p>
                     <p>Lý thuyết 2: <a target="__blank" href="https://s3.cloud.cmctelecom.vn/tvu/IT/220060_HeQuanTriCSDL/Video/Ly%20thuyet/Chuong4_P2.mp4">Click here</a></p>
