2663da84e486e1beee95f753331512e204a1f373
mqthang_18sh <maiquocthangt@gmail.com>
2023-07-15 19:37:25 +0700

    Update

--- data/content/CSTTNT/CSTTNT4.js
+++ data/content/CSTTNT/CSTTNT4.js
@@ -1,44 +1,233 @@
 const CSTTNT4 = {
     CSTTNT01: {
-        title: '',
+        title: 'Sự cần thiết của tri thiết trong giai quyết vấn đề',
         index: [
-
+            ["Giới thiệu", "CSTTNT011", `
+                <center>
+                    <video class="col-12" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=75,238" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Biểu diễn tri thức", "CSTTNT012", `
+                <center>
+                    <video class="col-12" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=238,336" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Logic", "CSTTNT013", `
+            <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=336,427" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `]
         ]
-    }, 
+    },
     CSTTNT02: {
-        title: '',
+        title: 'Logic mệnh đề',
         index: [
-
+            ["Giới thiệu", "CSTTNT021", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=427,468" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Ký hiệu", "CSTTNT022", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=468,518" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Các câu hay công thức", "CSTTNT023", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=518,669" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Ngữ nghĩa", "CSTTNT024", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=669,917" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Các công thức tương đương", "CSTTNT025", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=917,1048" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
+ 
         ]
-    }, 
+    },
     CSTTNT03: {
-        title: '',
+        title: 'Suy diễn với logic mệnh đề',
         index: [
-
+            ["Giới thiệu", "CSTTNT031", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1048,1198" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `],
+            ["Suy diễn với logic mệnh đề", "CSTTNT032", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1198,1366" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `],
+            ["Sử dụng quy tắc suy diễn", "CSTTNT033", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1366,1598" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `],
         ]
-    }, 
+    },
     CSTTNT04: {
-        title: '',
+        title: 'Logic vị từ',
         index: [
+            ["Giới thiệu", "CSTTNT041", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1598,1715" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `],
+            ["Các ký hiệu và ý nghĩa", "CSTTNT042", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1715,1816" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `],
+            ["Hạng thức (TERM)", "CSTTNT043", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1816,1950" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Câu nguyên tử, câu đơn", "CSTTNT044", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=1950,2187" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Lượng tử", "CSTTNT045", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2187,2401" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Literal", "CSTTNT046", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2401,2458" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
 
+            ["Các công thức tương đương", "CSTTNT047", `
+                <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2458,2574" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `]
         ]
-    }, 
+    },
     CSTTNT05: {
-        title: '',
+        title: 'Suy diễn với logic vị từ',
         index: [
-
+            ["Giới thiệu", "CSTTNT051", `
+            <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2574,2624" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Các qui tắc suy diễn", "CSTTNT052", `
+            <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2624,2880" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Suy diễn tiến và suy diễn lùi", "CSTTNT053", `
+            <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2880,2966" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `],
+            ["Suy diễn sử dụng phép giải", "CSTTNT054", `
+            <center>
+                    <video class="col-12" playbackRate="2" controls>
+                        <source 
+                        src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4#t=2966,3016" 
+                        type="video/mp4" />
+                    </video>
+                </center>
+            `]
         ]
-    }, 
+    },
     CSTTNT06: {
-        title: '',
-        index: [
-
-        ]
-    }, 
-    CSTTNT04: {
         title: 'Video bài giảng',
         index: [
-            ["TVU", "CSTTNT041", `
+            ["TVU", "CSTTNT061", `
                 <p>Link: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai04.mp4" target="_blank">Link</a></p>
             `]
         ]
--- data/content/CSTTNT/CSTTNT5.js
+++ data/content/CSTTNT/CSTTNT5.js
@@ -2,15 +2,102 @@
 
 const CSTTNT5 = {
     CSTTNT01: {
-        title: '',
+        title: 'Vấn đề thông tin không chắc chắn khi suy diễn',
         index: [
-
+            ["Giới thiệu và nguyên nhân", "CSTTNT011", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=59,238" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `],
+            ["Cách giải quyết", "CSTTNT012", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=238,305" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
         ]
-    }, 
+    },
+    CSTTNT02: {
+        title: 'Nguyên tắc lập luận xác suất',
+        index: [
+            ["Giới thiệu", "CSTTNT021", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=305,567" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
+        ]
+    },
+    CSTTNT03: {
+        title: 'Một số khái niệm về xác xuất',
+        index: [
+            ["Giới thiệu", "CSTTNT031", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=567,1197" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
+        ]
+    },
     CSTTNT04: {
+        title: 'Mạng Bayes và biểu diễn bài toàn thông qua mạng Bayes',
+        index: [
+            ["Giới thiệu", "CSTTNT041", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=1197,1694" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
+        ]
+    },
+    CSTTNT05: {
+        title: 'Suy diễn bằng mạng Bayes',
+        index: [
+            ["Giới thiệu", "CSTTNT051", `
+            <center>
+                <video class="col-12" playbackRate="2" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=1694,2008" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
+        ]
+    },
+    CSTTNT06: {
+        title: 'Ứng dụng của suy diễn xác xuất',
+        index: [
+            ["Giới thiệu", "CSTTNT061", `
+            <center>
+                <video class="col-12" controls>
+                    <source 
+                    src="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4#t=2008" 
+                    type="video/mp4" />
+                </video>
+            </center>
+            `]
+        ]
+    },
+    CSTTNT07: {
         title: 'Video bài giảng',
         index: [
-            ["TVU", "CSTTNT041", `
+            ["TVU", "CSTTNT071", `
                 <p>Link: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220242_CoSoTriTueNhanTao/Video/LyThuyet/Bai05.mp4" target="_blank">Link</a></p>
             `]
         ]
