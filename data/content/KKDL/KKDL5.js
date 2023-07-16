8cb552e4d6d8becf95e903b9b2b99401b00c639a
mqthang_18sh <maiquocthangt@gmail.com>
2023-07-16 19:41:13 +0700

    Update

--- data/content/KKDL/KKDL5.js
+++ data/content/KKDL/KKDL5.js
@@ -1,9 +1,223 @@
 const KKDL5 = {
-    
+    KKDL01: {
+        title: 'Bài toán phân lớp',
+        index: [
+            ["Giới thiệu", "KKDL011", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=130,190" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["Phân lớp nhị phân, đa lớp", "KKDL012", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=190,261" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["Ví dụ", "KKDL013", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=261,283" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+        ]
+    },
+    KKDL02: {
+        title: 'Qui trình xây dựng mô hình phân lớp',
+        index: [
+            ["Các bước thực hiện", "KKDL021", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=283,516" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["Nghi thức đánh giá", "KKDL022", `
+            <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=516,619" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["Các độ đo mô hình phân lớp", "KKDL023", `
+            <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=619,760" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+        ]
+    },
+    KKDL03: {
+        title: 'Giải thuật kNN',
+        index: [
+            ["Giới thiệu", "KKDL031", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=760,846" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+            ["Ví dụ", "KKDL032", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=846,956" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+            ["Các khoảng cách trong giải thuật KNN", "KKDL033", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=956,1016" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+            ["Ví dụ", "KKDL034", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1016,1156" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+            ["Giải thuật KNN", "KKDL035", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1156,1220" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+        ]
+    },
+    KKDL04: {
+        title: 'Giải thuật cây quyết định',
+        index: [
+            ["Giới thiệu", "KKDL041", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1220,1339" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+            ["Ví dụ", "KKDL042", `
+            <center>
+            <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1339,1510" type="video/mp4"/>
+            </video>
+        </center>
+            `],
+            ["Giải thuật", "KKDL043", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1510,1579" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["Chọn thuộc tính phân hoạch", "KKDL044", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1579,1699" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["Độ lợi thông tin", "KKDL045", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=1699,2147" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["Chỉ số Gini và các loại giải thuật", "KKDL048", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=2147,2246" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["Ví dụ", "KKDL049", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=2246,2342" type="video/mp4"/>
+                </video>
+            </center>
+            `]
+        ]
+    },
+    KKDL05: {
+        title: 'Giải thuật Bayes',
+        index: [
+            ["Giới thiệu", "KKDL051", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=2342,2460" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["Ví dụ", "KKDL052", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=2460,2877" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+        ]
+    },
+    KKDL06: {
+        title: 'Giải thuật máy học vector hỗ trợ',
+        index: [
+            ["Giới thiệu", "KKDL061", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=2877,2965" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["Ví dụ", "KKDL062", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=2965,3220" type="video/mp4"/>
+                </video>
+            </center>
+            `],
+            ["SV phi tuyến, ứng dụng và ưu nhược điểm", "KKDL063", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=3220,3494" type="video/mp4"/>
+                </video>
+            </center>
+            `]
+        ]
+    },
     KKDL07: {
+        title: 'Phương pháp kết hợp mô hình',
+        index: [
+            ["Giới thiệu", "KKDL071", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=3494,3881" type="video/mp4"/>
+                </video>
+            </center>
+            `]
+        ]
+    },
+    KKDL08: {
+        title: 'Bài toán hồi quy',
+        index: [
+            ["Giới thiệu", "KKDL071", `
+            <center>
+                <video class="col-12" controls>
+                    <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4#t=3881" type="video/mp4"/>
+                </video>
+            </center>
+            `]
+        ]
+    },
+    KKDL09: {
         title: 'Video bài giảng',
         index: [
-            ["Video bài giảng", "KKDL071", `
+            ["Video bài giảng", "KKDL091", `
                 <p>Lý thuyết: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4" target="__blank">Link</a></p>
 
                 <p>Thực hành: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Thuc%20hanh/TH5.mp4" target="__blank">Link</a></p>
