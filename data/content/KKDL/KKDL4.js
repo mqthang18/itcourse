ec27fe624ab694a5e3cc550005d55a2db1bf9058
mqthang_18sh <maiquocthangt@gmail.com>
2023-07-16 18:17:34 +0700

    Update

--- data/content/KKDL/KKDL4.js
+++ data/content/KKDL/KKDL4.js
@@ -1,8 +1,184 @@
 const KKDL4 = {
     KKDL01: {
+        title: 'Các khái niệm cơ bản',
+        index: [
+            ["Bài toán phân tích vỏ hàng", "KKDL011", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=94,284" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+        ]
+    },
+    KKDL02: {
+        title: 'Phân loại luật kết hợp',
+        index: [
+            ["ASSOCIATION (SỰ KẾT HỢP) VÀ ASSOCIATION RULE (LUẬT KẾT HỢP)", "KKDL012", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=284,340" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["VÍ DỤ VỀ LUẬT KẾT HỢP", "KKDL013", `
+            <center>
+                <video class="col-12" controls>
+                <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=340,534" type="video/mp4"/>
+                </video>
+            </center>
+        `],
+            ["LUẬT KẾT HỢP", "KKDL014", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=534,572" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+      
+            
+            ["ĐỘ HỖ TRỢ VÀ ĐỘ TIN CẬY", "KKDL015", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=572,710" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["MINIMUM SUPPORT VÀ MINIMUM CONFIDENCE", "KKDL016", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=710,860" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["FREQUENT ITEMSET (TẬP PHẦN TỬ PHỔ BIẾN)", "KKDL017", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=860,880" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["STRONG ASSOCIATION RULE (LUẬT KẾT HỢP MẠNH)", "KKDL018", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=880,1006" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["MỘT SỐ LOẠI LUẬT KẾT HỢP", "KKDL019", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=1006,1141" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["THUẬT TOÁN SINH CÁC LUẬT KẾT HỢP", "KKDL0120", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=1141,1265" type="video/mp4"/>
+                    </video>
+                </center>
+            `]
+        ]
+    },
+    KKDL03: {
+        title: 'Giải thuật Apriori',
+        index: [
+            ["APRIORI ALGORITHM", "KKDL0131", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=1265,1403" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["Ví dụ", "KKDL0132", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=1403,1930" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["Hạn chế của APRIORI", "KKDL0133", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=1930,1973" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["CẢI TIẾN APRIORI", "KKDL0134", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=1973,2001" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            
+        ]
+    },
+    KKDL04: {
+        title: 'Giải thuật FP-Growth',
+        index: [
+            ["GIẢI THUẬT FP-GROWTH", "KKDL0135", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2001,2066" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["XÂY DỰNG FP-TREE", "KKDL0136", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2066,2210" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["VÍ DỤ", "KKDL0137", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2210,2470" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["LỢI ÍCH CỦA CẤU TRÚC FP-TREE", "KKDL0138", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2470,2505" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            
+        ]
+    },
+    KKDL05: {
+        title: 'Khai thác luật kết hợp',
+        index: [
+            ["KHAI THÁC LUẬT KẾT HỢP ĐA CHIỀU", "KKDL0141", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2505,2565" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["KHAI THÁC LUẬT KẾT HỢP ĐA MỨC", "KKDL0151", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2565,2741" type="video/mp4"/>
+                    </video>
+                </center>
+            `],
+            ["KHAI THÁC LUẬT KẾT HỢP DỰA TRÊN RÀNG BUỘC", "KKDL0161", `
+                <center>
+                    <video class="col-12" controls>
+                        <source src="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4#t=2741,2862" type="video/mp4"/>
+                    </video>
+                </center>
+            `]
+        ]
+    },
+    KKDL07: {
         title: 'Video bài giảng',
         index: [
-            ["Video bài giảng", "KKDL011", `
+            ["Video bài giảng", "KKDL071", `
                 <p>Lý thuyết: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong04.mp4" target="__blank">Link</a></p>
 
                 <p>Thực hành: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Thuc%20hanh/TH4.mp4" target="__blank">Link</a></p>
--- data/content/KKDL/KKDL5.js
+++ data/content/KKDL/KKDL5.js
@@ -1,8 +1,9 @@
 const KKDL5 = {
-    KKDL01: {
+    
+    KKDL07: {
         title: 'Video bài giảng',
         index: [
-            ["Video bài giảng", "KKDL011", `
+            ["Video bài giảng", "KKDL071", `
                 <p>Lý thuyết: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Ly%20thuyet/Chuong05.mp4" target="__blank">Link</a></p>
 
                 <p>Thực hành: <a href="https://s3.cloud.cmctelecom.vn/tvu/IT/220109_Khaikhoangdulieu/Video/Thuc%20hanh/TH5.mp4" target="__blank">Link</a></p>
