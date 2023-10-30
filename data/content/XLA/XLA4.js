f8a0589f3d278b452de3fb201faa8dc6cbb8d37c
mqthang_18sh <maiquocthangt@gmail.com>
2023-10-30 20:08:35 +0700

    Update

--- data/content/XLA/XLA4.js
+++ data/content/XLA/XLA4.js
@@ -1,11 +1,130 @@
 // https://s3.cloud.cmctelecom.vn/tvu/IT/220057_XuLyAnh/Video/Chuong04.mp4
 
+let url_XLA4 = ''
 
 XLA4 = {
     XLA01: {
-        title: '',
+        title: 'Miền Fourier',
         index: [
-            
+            ["Giới thiệu miền Fourier", "XLA011", `
+
+            <div id="accordion">
+                <div class="card">
+                    <div class="card-header">
+                        <a class="btn" data-bs-toggle="collapse" href="#collapse11">
+                            Video
+                        </a>
+                    </div>
+                    <div id="collapse11" class="collapse" data-bs-parent="#accordion">
+                        <div class="card-body">
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="accordion">
+                <div class="card">
+                    <div class="card-header">
+                        <a class="btn" data-bs-toggle="collapse" href="#collapse12">
+                            Lý thuyết
+                        </a>
+                    </div>
+                    <div id="collapse12" class="collapse" data-bs-parent="#accordion">
+                        <div class="card-body">
+                            <ul>
+                                <li>Jean Baptiste Joseph Fourier là nhà toán học người Pháp và ông đã giới thiệu thuật toán Biến đổi Fourier vào năm 1805.</li>
+                                <li>Fourier đã phát hiện ra rằng một tín hiệu có thể được biểu diễn dưới dạng tổng các sóng hài cơ bản, mỗi sóng có một tần số và biên độ
+                                khác nhau.</li>
+                                <li>Ông đã sử dụng khái niệm này để giải quyết các vấn đề liên quan đến truyền dẫn nhiệt và sóng.</li>
+                                <li>Công trình nổi tiếng nhất của Fourier là "The Analytical Theory of Heat", được xuất bản bởi ông vào năm 1822.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `],
+            ["Chuỗi Fourier, lý do cần xử lý ảnh trong miền tần số, ứng dụng của Fourier", "XLA012", `
+                <div id="accordion">
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#collapse21">
+                                Miền Fourier
+                            </a>
+                        </div>
+                        <div id="collapse21" class="collapse" data-bs-parent="#accordion">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#collapse23">
+                                Chuỗi Fourier
+                            </a>
+                        </div>
+                        <div id="collapse23" class="collapse" data-bs-parent="#accordion">
+                            <div class="card-body">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#collapse24">
+                                Lý do cần xử lý ảnh trong miền tần số
+                            </a>
+                        </div>
+                        <div id="collapse24" class="collapse" data-bs-parent="#accordion">
+                            <div class="card-body">
+                                
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#collapse25">
+                                Ứng dụng của Fourier
+                            </a>
+                        </div>
+                        <div id="collapse25" class="collapse" data-bs-parent="#accordion">
+                            <div class="card-body">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `],
+            ["Các khái niệm cơ bản trong miền tần số Fourier", "XLA013", ``],
+            ["Biến đổi Fourier rời rạc (DFT) và biến đổi Fourier liên tục", "XLA014", ``],
+            ["Các kỹ thuật lọc trong miền tần số Fourier", "XLA015", ``]
+        ]
+    },
+    XLA02: {
+        title: 'Sự tương ứng lọc trong miền không gian và miền tần số',
+        index: [
+
+        ]
+    },
+    XLA03: {
+        title: 'Kỹ thuật làm trơn ảnh sử dụng bộ lọc trong miền tần số',
+        index: [
+
+        ]
+    },
+    XLA04: {
+        title: 'Kỹ thuật làm sắc nét ảnh sử dụng bộ lọc trong miền tần số',
+        index: [
+
+        ]
+    },
+    XLA05: {
+        title: 'Bộ lọc Homomorphic',
+        index: [
+
         ]
     }
 }
