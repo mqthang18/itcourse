a28d6f1a542b1a0c403387092991cf1e13d44202
mqthang_18sh <maiquocthangt@gmail.com>
2023-10-31 20:09:20 +0700

    Update

--- data/content/MHUD/MHUD5.js
+++ data/content/MHUD/MHUD5.js
@@ -1,22 +1,33 @@
+
 let MHUD5 = {
     MHUD01: {
-        title: '',
-        index: []
+        title: 'Trích rút đặc trưng và mã hóa đặc trưng',
+        index: [
+            ["Video", "MHUD011", VideoScript('https://s3.cloud.cmctelecom.vn/tvu/IT/220248_MayHocUngDung/Video/Bai5/5.1.mp4')]
+        ]
     },
     MHUD02: {
-        title: '',
-        index: []
+        title: 'Principal Component Analysis (PCA)',
+        index: [
+            ["Video", "MHUD021", VideoScript('https://s3.cloud.cmctelecom.vn/tvu/IT/220248_MayHocUngDung/Video/Bai5/5.2.mp4')]
+        ]
     },
     MHUD03: {
-        title: '',
-        index: []
+        title: 'Linear Discriminant Analysis (LDA)',
+        index: [
+            ["Video", "MHUD031", VideoScript('https://s3.cloud.cmctelecom.vn/tvu/IT/220248_MayHocUngDung/Video/Bai5/5.3.mp4')]
+        ]
     },
     MHUD04: {
-        title: '',
-        index: []
+        title: 'Singular Value Decomposition (SVD)',
+        index: [
+            ["Video", "MHUD041", VideoScript('https://s3.cloud.cmctelecom.vn/tvu/IT/220248_MayHocUngDung/Video/Bai5/5.4.mp4')]
+        ]
     },
     MHUD05: {
-        title: '',
-        index: []
+        title: 'Kết thúc bài',
+        index: [
+            ["Video", "MHUD051", VideoScript('https://s3.cloud.cmctelecom.vn/tvu/IT/220248_MayHocUngDung/Video/Bai5/KTB.mp4')]
+        ]
     },
 }
